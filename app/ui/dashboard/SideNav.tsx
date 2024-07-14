0112f25458a52082d1d72cbcaffee3d63d3d8150
glitzypanic <jose.fariasdv@gmail.com>
2024-07-13 23:55:35 -0400

    feat: Add SideNav component to dashboard

---
+++ app/ui/dashboard/SideNav.tsx
@@ -0,0 +1,11 @@
+export default function SideNav() {
+  return (
+    <nav className="bg-gray-800 w-64 h-full">
+      <ul>
+        <li className="text-white">Dashboard</li>
+        <li className="text-white">Transactions</li>
+        <li className="text-white">Categories</li>
+      </ul>
+    </nav>
+  );
+}
