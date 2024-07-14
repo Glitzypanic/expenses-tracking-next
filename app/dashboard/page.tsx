9c9017fe7218d2121bc4c40f902d27dc63612429
glitzypanic <jose.fariasdv@gmail.com>
2024-07-14 01:50:27 -0400

    feat: Add layout and dashboard page components

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+export default function Page() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-5xl">Dashboard page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id
+        reprehenderit non iste explicabo aspernatur, alias cumque fuga adipisci
+        quam! Reprehenderit similique adipisci quo deleniti ea possimus quae
+        porro eos modi?
+      </p>
+    </main>
+  );
+}
