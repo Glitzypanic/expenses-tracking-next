314c4852c86899c86d9cc0bdf0e829f98db52136
glitzypanic <jose.fariasdv@gmail.com>
2024-07-14 02:02:16 -0400

    chore: Update SideNav component with new menu items and styling

--- app/ui/dashboard/SideNav.tsx
+++ app/ui/dashboard/SideNav.tsx
@@ -5,10 +5,19 @@ export default function SideNav() {
         <img className="rounded-lg" src="Logo-xs.png" alt="Company logo" />
         <span className=" text-3xl font-bold">GTracking</span>
       </header>
-      <ul className="text-gray-700 px-5 text-lg">
-        <li className="py-2 px-4 hover:bg-gray-200">Dashboard</li>
-        <li className="py-2 px-4 hover:bg-gray-200">Transactions</li>
-        <li className="py-2 px-4 hover:bg-gray-200">Categories</li>
+      <ul className="flex flex-col gap-2 text-gray-700 px-5 text-lg">
+        <li className="flex gap-2 py-2 px-4 hover:bg-gray-200 cursor-pointer hover:rounded-lg">
+          <img src="Dashboard.svg" alt="" width={25} />
+          Dashboard
+        </li>
+        <li className="flex gap-2 py-2 px-4 hover:bg-gray-200 cursor-pointer hover:rounded-lg">
+          <img src="Expenses.svg" alt="" width={25} />
+          Expenses
+        </li>
+        <li className="flex gap-2 py-2 px-4 hover:bg-gray-200 cursor-pointer hover:rounded-lg">
+          <img src="Report.svg" alt="" width={25} />
+          Reports
+        </li>
       </ul>
     </nav>
   );
