3ab0a9b6a2b102d06aa42a27256a9be561e7cbbb
glitzypanic <jose.fariasdv@gmail.com>
2024-07-13 23:56:04 -0400

    chore: Add layout and landing page components

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-5xl">Landing page</h1>
+    </main>
+  );
+}
