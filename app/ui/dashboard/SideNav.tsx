9c00c90d593e9e81469a02dda03733bddcdf2b8e
glitzypanic <jose.fariasdv@gmail.com>
2024-07-14 01:50:37 -0400

    chore: Update SideNav component styling and add logo

--- app/ui/dashboard/SideNav.tsx
+++ app/ui/dashboard/SideNav.tsx
@@ -1,10 +1,14 @@
 export default function SideNav() {
   return (
-    <nav className="bg-gray-800 w-64 h-full">
-      <ul>
-        <li className="text-white">Dashboard</li>
-        <li className="text-white">Transactions</li>
-        <li className="text-white">Categories</li>
+    <nav className="w-64 h-full border-r-2">
+      <header className="flex justify-center items-center gap-2 py-10">
+        <img className="rounded-lg" src="Logo-xs.png" alt="Company logo" />
+        <span className=" text-3xl font-bold">GTracking</span>
+      </header>
+      <ul className="text-gray-700 px-5 text-lg">
+        <li className="py-2 px-4 hover:bg-gray-200">Dashboard</li>
+        <li className="py-2 px-4 hover:bg-gray-200">Transactions</li>
+        <li className="py-2 px-4 hover:bg-gray-200">Categories</li>
       </ul>
     </nav>
   );
