3642b36015c3f950d58ec733fab29b163e077a3e
glitzypanic <jose.fariasdv@gmail.com>
2024-07-19 19:00:34 -0400

    feat: Add MockData utility for generating mock data

---
+++ lib/utils/MockData.ts
@@ -0,0 +1,55 @@
+import { MockData } from "../types/interfaces";
+
+const mockData: MockData[] = [
+  {
+    id: 1,
+    order: "Spotify",
+    amount: "$9.99",
+    date: "12 Apr, 2024",
+    payment: "Paid",
+  },
+  {
+    id: 2,
+    order: "Netflix",
+    amount: "$14.99",
+    date: "12 Apr, 2024",
+    payment: "Unpaid",
+  },
+  {
+    id: 3,
+    order: "Amazon",
+    amount: "$99.99",
+    date: "12 Apr, 2024",
+    payment: "Unpaid",
+  },
+  {
+    id: 4,
+    order: "Spotify",
+    amount: "$9.99",
+    date: "12 Apr, 2024",
+    payment: "Paid",
+  },
+  {
+    id: 5,
+    order: "Netflix",
+    amount: "$14.99",
+    date: "12 Apr, 2024",
+    payment: "Paid",
+  },
+  {
+    id: 6,
+    order: "Netflix",
+    amount: "$14.99",
+    date: "12 Apr, 2024",
+    payment: "Unpaid",
+  },
+  {
+    id: 7,
+    order: "Netflix",
+    amount: "$14.99",
+    date: "12 Apr, 2024",
+    payment: "Paid",
+  },
+];
+
+export { mockData };
