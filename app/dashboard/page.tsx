65ce4868a5c1dfececade9184f0ac28759f093c5
glitzypanic <jose.fariasdv@gmail.com>
2024-07-18 22:02:21 -0400

    chore: Update dashboard page layout and add TableDaily component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,41 +3,51 @@ import PieChart from "../components/ui/PieChart";
 import CardOverview from "../components/ui/CardOverview";
 import SavingGoal from "../components/ui/SavingGoal";
 import Upcoming from "../components/ui/Upcoming";
-import { Pi } from "lucide-react";
+import TableDaily from "../components/ui/TableDaily";
 
 export default function Page() {
   return (
-    <main className="bg-[#1A1A1A] flex  ml-5 mr-5">
-      <section className="flex">
-        <LineChart />
+    <main className="bg-[#1A1A1A] flex m-5 h-full">
+      <section className="flex-grow grid grid-cols-[1fr,auto] mr-5">
+        <div className="grid grid-cols-[1fr,auto] h-fit">
+          <div>
+            <LineChart />
+          </div>
+
+          <div className="grid grid-cols-2 gap-x-3 gap-y-5 h-fit">
+            <CardOverview
+              title={"Total Balance"}
+              value={"7,400"}
+              percentage={" 5,4%"}
+              description={"last month"}
+            />
+            <CardOverview
+              title={"Monthly Expenses"}
+              value={"10,400"}
+              percentage={" 10%"}
+              description={"last month"}
+            />
+            <CardOverview
+              title={"Remaining Budget"}
+              value={"15,000"}
+              percentage={" 15%"}
+              description={"used budget"}
+            />
+            <CardOverview
+              title={"Savings"}
+              value={"15,000"}
+              percentage={" 15%"}
+              description={"last month"}
+            />
+          </div>
+
+          <div className="bg-[#0A0A0A] rounded-lg col-span-2 mt-4 p-5 h-full">
+            <TableDaily />
+          </div>
+        </div>
       </section>
-      <section className="grid grid-cols-2 h-fit w-fit gap-5">
-        <CardOverview
-          title={"Total Balance"}
-          value={"7,400"}
-          percentage={" 5,4%"}
-          description={"last month"}
-        />
-        <CardOverview
-          title={"Monthly Expenses"}
-          value={"10,400"}
-          percentage={" 10%"}
-          description={"last month"}
-        />
-        <CardOverview
-          title={"Remaining Budget"}
-          value={"15,000"}
-          percentage={" 15%"}
-          description={"used budget"}
-        />
-        <CardOverview
-          title={"Savings"}
-          value={"15,000"}
-          percentage={" 15%"}
-          description={"last month"}
-        />
-      </section>
-      <aside className="ml-5 w-[525px]">
+
+      <aside className="w-[525px]">
         <PieChart />
         <SavingGoal />
         <Upcoming />
