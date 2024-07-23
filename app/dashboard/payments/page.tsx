32d58b0b6c22877daf2da3b13a1fa0fe18030650
glitzypanic <jose.fariasdv@gmail.com>
2024-07-23 18:20:00 -0400

    refactor: Update PaymentsPage component to display 15 user payment records and add pagination

--- app/dashboard/payments/page.tsx
+++ app/dashboard/payments/page.tsx
@@ -1,41 +1,52 @@
 import { generateData, User } from "@/lib/utils/generateData";
+import { Pagination, PaginationCursor } from "@nextui-org/pagination";
 import clsx from "clsx";
 
 const PaymentsPage = () => {
-  const users: User[] = generateData(10); // Genera 10 registros de ejemplo
+  const users: User[] = generateData(15); // Genera 10 registros de ejemplo
 
   return (
-    <div className="bg-[#0A0A0A] rounded-lg col-span-2 p-5 h-full m-5">
-      <h1>Lista de Usuarios</h1>
-      <table className="min-w-full text-left mt-7 mb-[26px]">
+    <div className="bg-[#0A0A0A] rounded-lg col-span-2 px-6 h-full m-5">
+      <h2 className="text-4xl font-bold pt-6">Transactions</h2>
+      <table className="min-w-full text-left mt-7 mb-[26px] border-b border-white">
         <thead className="border border-white">
           <tr>
-            <th className="py-3 px-4">ID</th>
-            <th className="py-3 px-4">Nombre</th>
-            <th className="py-3 px-4">Edad</th>
-            <th className="py-3 px-4">Ocupación</th>
+            <th className="py-3 px-4">Id</th>
+            <th className="py-3 px-4">Status</th>
+            <th className="py-3 px-4">Name</th>
+            <th className="py-3 px-4">Amount</th>
+            <th className="py-3 px-4">Date</th>
           </tr>
         </thead>
         <tbody className="divide-y divide-gray-200">
           {users.map((user) => (
             <tr key={user.id}>
-              <td className="py-2 px-4">{user.id}</td>
-              <td className="py-2 px-4">{user.order}</td>
-              <td className="py-2 px-4">{user.amount}</td>
+              <td className="py-2 px-4 text-[#9ca3af]">#{user.id}</td>
               <div className="py-2">
                 <td
                   className={clsx("rounded-lg flex w-fit px-3 font-medium", {
-                    "bg-[#c4f9e4]  text-[#28895e]": user.payment === "Paid",
-                    "bg-[#dfd3ff] text-[#7d62ca]": user.payment === "Unpaid",
+                    "bg-[#c4f9e4]  text-[#28895e]": user.status === "Paid",
+                    "bg-[#dfd3ff] text-[#7d62ca]": user.status === "Unpaid",
                   })}
                 >
-                  {user.payment}
+                  {user.status}
                 </td>
               </div>
+              <td className="py-2 px-4">{user.name}</td>
+              <td className="py-2 px-4">$ {user.amount}</td>
+              <td className="py-2 px-4">{user.date}</td>
             </tr>
           ))}
         </tbody>
       </table>
+      <div className="flex justify-end">
+        <Pagination
+          total={5}
+          initialPage={1}
+          classNames={{ cursor: "bg-[#CCFF00]" }}
+          showControls
+        />
+      </div>
     </div>
   );
 };
