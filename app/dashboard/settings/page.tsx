fdc26865d00c9d72d0102839798064e5483cf8e4
glitzypanic <jose.fariasdv@gmail.com>
2024-07-24 16:23:57 -0400

    refactor: Update Settings page layout and heading style

--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
-    <div>
-      <h1>Settings</h1>
+    <div className="bg-[#0A0A0A] rounded-lg col-span-2 px-6 h-full m-5 p-5">
+      <h2 className="text-3xl font-bold pt-6">Settings</h2>
     </div>
   );
 }
