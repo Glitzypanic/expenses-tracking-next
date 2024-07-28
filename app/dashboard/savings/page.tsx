dc0cd3e009b84760142c6e0e9af93b3628516a3f
glitzypanic <jose.fariasdv@gmail.com>
2024-07-27 21:04:47 -0400

    refactor: Update SavingsPage component to include PieChart and savings calculator

--- app/dashboard/savings/page.tsx
+++ app/dashboard/savings/page.tsx
@@ -1,27 +1,26 @@
-import { Progress } from "@nextui-org/react";
-import PieChart from "../../components/ui/PieChart";
+import PieChart from "../../components/ui/dashboard/PieChart";
+import NewGoal from "@/app/components/ui/savings/NewGoal";
+import CurrGoal from "@/app/components/ui/savings/CurrGoal";
+import SavingCalc from "@/app/components/ui/savings/SavingCalc";
 
 export default function ExpensesPage() {
   return (
-    <div className="bg-[#0A0A0A] rounded-lg col-span-2 px-6 h-full m-5 p-5">
-      <h2 className="text-3xl font-bold pt-6">Savings</h2>
-      <PieChart />
-      <div className="w-full border-2 h-32 flex flex-col justify-center p-5">
-        <h3 className="font-semibold text-2xl">Saving goals calculator</h3>
-        <label className="w-fit inline-flex gap-3 mt-3">
-          <input
-            type="text"
-            className="p-2 rounded-lg"
-            placeholder="Saving Goal"
-          />
-          <input
-            type="text"
-            className="p-2 rounded-lg"
-            placeholder="Number of Month"
-          />
-        </label>
-        <p className="mt-2 font-medium">Month savings necessary: $0</p>
-      </div>
+    <div className=" col-span-2 h-full mx-5 mt-5">
+      <section className="grid grid-cols-2 gap-3 w-fit">
+        <div className="bg-[#0A0A0A] rounded-lg p-5">
+          <h2 className="text-3xl font-bold">Savings</h2>
+          <PieChart />
+        </div>
+        <div className="bg-[#0A0A0A] rounded-lg">
+          <CurrGoal />
+        </div>
+        <div className="bg-[#0A0A0A] rounded-lg">
+          <NewGoal />
+        </div>
+        <div className="bg-[#0A0A0A] rounded-lg">
+          <SavingCalc />
+        </div>
+      </section>
     </div>
   );
 }
