c83a1ef989443015b825f7559245f724048e35a3
glitzypanic <jose.fariasdv@gmail.com>
2024-07-27 21:05:46 -0400

    refactor: Add NewGoal component for savings page

---
+++ app/components/ui/savings/NewGoal.tsx
@@ -0,0 +1,35 @@
+const NewGoal = () => {
+  return (
+    <div className="w-full h-32 flex flex-col justify-center p-5">
+      <h3 className="font-semibold text-2xl">Add new saving goal</h3>
+      <div>
+        <label className="w-fit inline-flex gap-3 mt-3">
+          <input
+            type="text"
+            className="p-2 rounded-lg"
+            name=""
+            id=""
+            placeholder="Name"
+          />
+          <input
+            type="number"
+            className="p-2 rounded-lg"
+            name=""
+            id=""
+            placeholder="Goal mount"
+          />
+          <input
+            type="number"
+            className="p-2 rounded-lg"
+            name=""
+            id=""
+            placeholder="Actual mount"
+          />
+        </label>
+        <button>Add</button>
+      </div>
+    </div>
+  );
+};
+
+export default NewGoal;
