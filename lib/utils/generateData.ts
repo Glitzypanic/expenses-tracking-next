42729fc849f11318f08322f039bd86b58b988cf7
glitzypanic <jose.fariasdv@gmail.com>
2024-07-30 19:07:50 -0400

    refactor: Update User interface id property to be of type number

--- lib/utils/generateData.ts
+++ lib/utils/generateData.ts
@@ -1,7 +1,7 @@
 import { faker } from "@faker-js/faker";
 
 export interface User {
-  id: string;
+  id: number;
   name: string;
   amount: string;
   date: string;
@@ -24,9 +24,9 @@ export const generateData = (numRecords: number) => {
     "mortgage",
   ];
   const data: User[] = [];
-  for (let i = 0; i < numRecords; i++) {
+  for (let i = 0; i < numRecords; i + 1) {
     data.push({
-      id: faker.random.alphaNumeric(5), // Genera un valor aleatorio de tipo "string" con 10 caracteres alfanuméricos
+      id: i,
       name: name[Math.floor(Math.random() * name.length)], // Genera un valor aleatorio de la lista "name"
       amount: faker.finance.amount(), // Genera un valor aleatorio de tipo "string" en formato "$123.45"
       date: faker.date.anytime().toISOString().split("T")[0], // Genera una fecha aleatoria y la convierte a un string en formato "YYYY-MM-DD"
