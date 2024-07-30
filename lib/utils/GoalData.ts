c8eeb5a4ffbfaecac3a6edc8ce00ef56cd5a968c
glitzypanic <jose.fariasdv@gmail.com>
2024-07-29 22:48:18 -0400

    feat: Add GoalData utility for goal options in savings page

---
+++ lib/utils/GoalData.ts
@@ -0,0 +1,6 @@
+export const GoalData = [
+  { key: "Vacations", label: "Vacations" },
+  { key: "Car", label: "Car" },
+  { key: "House", label: "House" },
+  { key: "Education", label: "Education" },
+];
