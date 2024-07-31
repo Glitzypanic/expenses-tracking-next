58c2711e3c4ecd0798f7bc844c221aee83d9de50
glitzypanic <jose.fariasdv@gmail.com>
2024-07-30 20:55:51 -0400

    refactor: Update SavingCalc component to use rounded-lg for background

--- app/components/ui/savings/SavingCalc.tsx
+++ app/components/ui/savings/SavingCalc.tsx
@@ -1,6 +1,6 @@
 const SavingCalc = () => {
   return (
-    <div className="w-full h-fit bg-[#0A0A0A] round flex flex-col justify-center p-5">
+    <div className="w-full h-fit bg-[#0A0A0A] rounded-lg flex flex-col justify-center p-5">
       <h3 className="font-semibold text-2xl">Calculate your savings</h3>
       <label className="w-fit inline-flex gap-3 mt-3">
         <input
