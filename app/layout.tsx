875c8e23239859b75436005d588d70e6d28742ea
glitzypanic <jose.fariasdv@gmail.com>
2024-07-30 22:49:37 -0400

    refactor: Update metadata icons property in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,7 @@ const roboto = Roboto({
 export const metadata: Metadata = {
   title: "GTracker",
   description: "A simple expense tracker",
-  viewport: "width=device-width, initial-scale=1",
-  icons: ["icon.ico"],
+  icons: "icon.ico",
 };
 
 export default function RootLayout({
