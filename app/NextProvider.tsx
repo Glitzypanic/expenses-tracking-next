e1aefb6120e01d1f43263ec66eb223a9f0e92960
glitzypanic <jose.fariasdv@gmail.com>
2024-08-04 17:16:17 -0400

    chore: Update npm dependencies for @chakra-ui/react and @emotion packages

--- app/NextProvider.tsx
+++ app/NextProvider.tsx
@@ -1,8 +1,7 @@
-// app/providers.tsx
 "use client";
 
 import { NextUIProvider } from "@nextui-org/react";
 
-export function Providers({ children }: { children: React.ReactNode }) {
+export function NextProviders({ children }: { children: React.ReactNode }) {
   return <NextUIProvider>{children}</NextUIProvider>;
 }
