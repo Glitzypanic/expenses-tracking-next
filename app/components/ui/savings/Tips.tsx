d81af3e6065ae947ffa5ffa86f1b451fc293586d
glitzypanic <jose.fariasdv@gmail.com>
2024-08-05 18:37:37 -0400

    feat: Add Stats and Tips components to SavingsPage

---
+++ app/components/ui/savings/Tips.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+
+export default function Tips() {
+  const tips = [
+    "Pay yourself first: Set aside savings before spending on non-essentials.",
+    "Track your expenses to identify areas where you can cut back.",
+    "Set specific, achievable savings goals to stay motivated.",
+    "Use the 50/30/20 rule: 50% needs, 30% wants, 20% savings.",
+    "Automate your savings to make it a habit.",
+  ];
+
+  const randomTip = tips[Math.floor(Math.random() * tips.length)];
+  return (
+    <div className="flex flex-col justify-center bg-[#7A306C] rounded-lg h-full p-5">
+      <Image
+        src={"/Euro.svg"}
+        alt="sdf"
+        width={300}
+        height={300}
+        className="place-self-center"
+      />
+      <p className="text-white mt-5">
+        <span className="font-bold">Savings Tip: </span>
+        {randomTip}
+      </p>
+    </div>
+  );
+}
--- app/dashboard/savings/page.tsx
+++ app/dashboard/savings/page.tsx
@@ -2,7 +2,9 @@ import PieChart from "../../components/ui/dashboard/PieChart";
 import NewGoal from "@/app/components/ui/savings/NewGoal";
 import CurrGoal from "@/app/components/ui/savings/CurrGoal";
 import SavingCalc from "@/app/components/ui/savings/SavingCalc";
-import { Calendar, colorVariants } from "@nextui-org/react";
+import { Calendar } from "@nextui-org/react";
+import { Stats } from "@/app/components/ui/savings/Stats";
+import Tips from "@/app/components/ui/savings/Tips";
 
 export default function ExpensesPage() {
   return (
@@ -17,17 +19,25 @@ export default function ExpensesPage() {
           <CurrGoal />
           <NewGoal />
         </div>
+        <div className="">
+          <Tips />
+        </div>
       </section>
-      <section className="mt-5 rounded-lg p-5 bg-[#0A0A0A] w-fit">
-        <Calendar
-          aria-label="Date (Visible Month)"
-          visibleMonths={3}
-          classNames={{
-            gridBody: "bg-[#0A0A0A]",
-            base: "shadow-none",
-            gridWrapper: "bg-[#0A0A0A]",
-          }}
-        />
+      <section className="flex mt-5">
+        <div className="rounded-lg p-5 bg-[#0A0A0A] w-fit">
+          <Calendar
+            aria-label="Date (Visible Month)"
+            visibleMonths={3}
+            classNames={{
+              gridBody: "bg-[#0A0A0A]",
+              base: "shadow-none",
+              gridWrapper: "bg-[#0A0A0A]",
+            }}
+          />
+        </div>
+        <div className="ml-5 bg-[#0A0A0A] w-fit p-5 h-fit rounded-lg flex items-center justify-center">
+          <Stats />
+        </div>
       </section>
     </div>
   );
