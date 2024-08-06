02ac376c2f908c0b8696997845e71c138e81b83f
glitzypanic <jose.fariasdv@gmail.com>
2024-08-05 21:14:32 -0400

    feat: Improve SavingsPage layout and add TopExpenses component

--- app/dashboard/savings/page.tsx
+++ app/dashboard/savings/page.tsx
@@ -5,12 +5,13 @@ import SavingCalc from "@/app/components/ui/savings/SavingCalc";
 import { Calendar } from "@nextui-org/react";
 import { Stats } from "@/app/components/ui/savings/Stats";
 import Tips from "@/app/components/ui/savings/Tips";
+import TopExpenses from "@/app/components/ui/savings/TopExpenses";
 
 export default function ExpensesPage() {
   return (
     <div className=" col-span-2 h-full mx-5 mt-5">
       <section className=" flex gap-5 rounded-lg w-fit">
-        <div className="bg-[#0A0A0A] rounded-lg p-5 w-fit">
+        <div className="bg-[#0A0A0A] rounded-lg p-5 w-[600px]">
           <h2 className="text-3xl font-bold">Savings</h2>
           <PieChart />
         </div>
@@ -24,6 +25,9 @@ export default function ExpensesPage() {
         </div>
       </section>
       <section className="flex mt-5">
+        <div className="mr-5 bg-[#0A0A0A] w-full p-5 rounded-lg">
+          <TopExpenses />
+        </div>
         <div className="rounded-lg p-5 bg-[#0A0A0A] w-fit">
           <Calendar
             aria-label="Date (Visible Month)"
@@ -34,9 +38,6 @@ export default function ExpensesPage() {
               gridWrapper: "bg-[#0A0A0A]",
             }}
           />
-        </div>
-        <div className="ml-5 bg-[#0A0A0A] w-fit p-5 h-fit rounded-lg flex items-center justify-center">
-          <Stats />
         </div>
       </section>
     </div>
