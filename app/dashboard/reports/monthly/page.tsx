d83f431eefd3832c9c52ad529054352140fb7f95
glitzypanic <jose.fariasdv@gmail.com>
2024-08-07 16:24:18 -0400

    refactor: Remove unused MonthlyPage and YearlyPage components

--- app/dashboard/reports/monthly/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function MonthlyPage() {
-  return (
-    <div>
-      <h1>Monthly Reports</h1>
-    </div>
-  );
-}
--- app/dashboard/reports/yearly/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function YearlyPage() {
-  return (
-    <div>
-      <h1>Yearly Reports</h1>
-    </div>
-  );
-}
