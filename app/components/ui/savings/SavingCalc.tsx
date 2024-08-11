f05fc81d5d9f80dec2ad860ca2346a06bcd61f73
glitzypanic <jose.fariasdv@gmail.com>
2024-08-11 15:38:06 -0400

    feat: Add the logic

--- app/components/ui/savings/SavingCalc.tsx
+++ app/components/ui/savings/SavingCalc.tsx
@@ -1,4 +1,28 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
 const SavingCalc = () => {
+  const [amountToSave, setAmountToSave] = useState("");
+  const [months, setMonths] = useState("");
+  const [monthySaving, setMonthySaving] = useState(0);
+
+  useEffect(() => {
+    const calculateSaving = () => {
+      const amount = parseFloat(amountToSave);
+      const numberOfMonths = parseInt(months);
+
+      if (!isNaN(amount) && !isNaN(numberOfMonths) && numberOfMonths > 0) {
+        const saving = Math.round(amount / numberOfMonths);
+        setMonthySaving(saving);
+      } else {
+        setMonthySaving(0);
+      }
+    };
+
+    calculateSaving();
+  }, [amountToSave, months]);
+
   return (
     <div className="w-full h-fit bg-[#0A0A0A] rounded-lg flex flex-col justify-center p-5">
       <h3 className="font-semibold text-2xl">Calculate your savings</h3>
@@ -6,17 +30,23 @@ const SavingCalc = () => {
         <input
           aria-label="Amount to save"
           className="p-2 rounded-lg focus:outline-[#CCFF00]"
-          type="text"
+          type="number"
+          value={amountToSave}
+          onChange={(e) => setAmountToSave(e.target.value)}
           placeholder="Amount to save"
         />
         <input
           aria-label="Number of months"
           className="p-2 rounded-lg focus:outline-[#CCFF00]"
-          type="text"
+          type="number"
+          value={months}
+          onChange={(e) => setMonths(e.target.value)}
           placeholder="Number of months"
         />
       </label>
-      <span className="mt-3 font-medium">Saving necessary: $0</span>
+      <span className="mt-3 font-medium">
+        Saving necessary: ${monthySaving}
+      </span>
     </div>
   );
 };
