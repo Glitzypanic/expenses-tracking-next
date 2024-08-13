9f09f2871785a9690e61f63762b7f8fb885fa52d
glitzypanic <jose.fariasdv@gmail.com>
2024-08-12 22:42:06 -0400

    Refactor: new data

--- lib/utils/GoalData.ts
+++ lib/utils/GoalData.ts
@@ -1,6 +1,40 @@
-export const GoalData = [
-  { key: "Vacations", label: "Vacations" },
-  { key: "Car", label: "Car" },
-  { key: "House", label: "House" },
-  { key: "Education", label: "Education" },
+import { GoalData } from "../types/interfaces";
+
+export const goalData: GoalData[] = [
+  {
+    id: 1,
+    name: "House",
+    goal: "500",
+    actual: "400",
+    progress: 40,
+    svg: "/assets/svgs/Home.svg",
+    alt: "House icon",
+  },
+  {
+    id: 2,
+    name: "New car",
+    goal: "500",
+    actual: "400",
+    progress: 40,
+    svg: "/assets/svgs/Car.svg",
+    alt: "Car icon",
+  },
+  {
+    id: 3,
+    name: "Vacations",
+    goal: "500",
+    actual: "400",
+    progress: 40,
+    svg: "/assets/svgs/Vacation.svg",
+    alt: "Vacation icon",
+  },
+  {
+    id: 4,
+    name: "Racing Simulator",
+    goal: "500",
+    actual: "400",
+    progress: 40,
+    svg: "/assets/svgs/Vacation.svg",
+    alt: "Vacation icon",
+  },
 ];
