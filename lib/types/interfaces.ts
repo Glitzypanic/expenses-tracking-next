7cd7e4074bcc93b246a1619eaa2d782bc9e4780a
glitzypanic <jose.fariasdv@gmail.com>
2024-08-12 22:44:36 -0400

    add GoalData interface

--- lib/types/interfaces.ts
+++ lib/types/interfaces.ts
@@ -5,3 +5,13 @@ export interface MockData {
   date: string;
   status: string;
 }
+
+export interface GoalData {
+  id: number;
+  name: string;
+  goal: string;
+  actual: string;
+  progress: number;
+  svg: string;
+  alt: string;
+}
