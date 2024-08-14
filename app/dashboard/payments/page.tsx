a1b77290b90bd6b3a7384fefba7241e855cba6ed
glitzypanic <jose.fariasdv@gmail.com>
2024-08-13 21:09:30 -0400

    Refactor and move to other folders

--- app/dashboard/payments/page.tsx
+++ app/dashboard/payments/page.tsx
@@ -1,4 +1,4 @@
-import { generateData, User } from "@/lib/utils/generateData";
+import { generateData, User } from "@/app/lib/utils/generateData";
 import { Pagination } from "@nextui-org/pagination";
 import clsx from "clsx";
 
