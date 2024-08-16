d9c8e3251721c75db8d181aba40765477544da78
glitzypanic <jose.fariasdv@gmail.com>
2024-08-15 23:47:07 -0400

    Feat: new pages to navigate on settings

---
+++ app/dashboard/settings/plans/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main className="bg-[#0A0A0A] mt-5 p-5 rounded-lg h-fit w-fit">
+      <h2 className="text-2xl font-bold">Choose Your Plan</h2>
+    </main>
+  );
+}
