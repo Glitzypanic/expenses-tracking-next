2611281a36e7214afc7d6bcb6741f8d2c806222e
glitzypanic <jose.fariasdv@gmail.com>
2024-08-15 23:45:44 -0400

    Feat: settings navbar

---
+++ app/components/ui/sidebar/SettingsNav.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import NavLink from "./NavLinks";
+import Link from "next/link";
+
+export default function SettingsNav() {
+  const navLinks = [
+    {
+      href: "/dashboard/settings/profile",
+      label: "Edit Profile",
+      icon: "/assets/svgs/Profile-w.svg",
+      activeIcon: "/assets/svgs/Profile.svg",
+    },
+    {
+      href: "/dashboard/settings/plans",
+      label: "Plans",
+      icon: "/assets/svgs/Wallet-w.svg",
+      activeIcon: "/assets/svgs/Wallet.svg",
+    },
+    {
+      href: "/dashboard/settings/help",
+      label: "Help & Support",
+      icon: "/assets/svgs/Help-w.svg",
+      activeIcon: "/assets/svgs/Help.svg",
+    },
+  ];
+
+  return (
+    <nav className="bg-[#0A0A0A] w-fit rounded-lg col-span-2 px-6 m-5 p-5">
+      <h2 className="text-2xl font-bold mb-10">
+        Update And Manage Your Account
+      </h2>
+      <ul className="flex flex-col gap-2 w-full text-white text-lg">
+        {navLinks.map((item) => (
+          <NavLink key={item.href} item={item} />
+        ))}
+      </ul>
+      <Link href={"/dashboard/settings/logout"}>
+        <button className="border-2 rounded-lg font-bold py-2 px-5 mt-24 w-full text-white hover:bg-red-500/10 hover:text-red-500 hover:border-red-500">
+          Logout
+        </button>
+      </Link>
+    </nav>
+  );
+}
