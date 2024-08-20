1c46a2f7d4c9a3cac47d7fa59e5420be78d4e7f5
glitzypanic <jose.fariasdv@gmail.com>
2024-08-19 22:28:29 -0400

    Refactor ReportsPage component layout and styles

--- app/dashboard/reports/page.tsx
+++ app/dashboard/reports/page.tsx
@@ -16,35 +16,39 @@ export default function ReportsPage() {
     financialTips[Math.floor(Math.random() * financialTips.length)];
 
   return (
-    <div className="rounded-lg h-full m-5">
-      <section className="mt-5 flex gap-5 justify-between">
-        <div className="w-[1000px]">
-          <ReportChart />
+    <section className="rounded-lg flex md:flex-col lg:flex-row gap-5 mb-5">
+      <div className="w-full">
+        <ReportChart />
+      </div>
+      <div className="w-full">
+        <div className="flex gap-5">
+          <TotalBalance />
+          <MonthlyExpenses />
         </div>
-        <div>
-          <div className="flex gap-10">
-            <TotalBalance />
-            <MonthlyExpenses />
-          </div>
-          <div className="flex gap-10 mt-5">
-            <button className="border-white border-2 p-4 hover:border-[#CCFF00] rounded-lg w-64 bg-[#0A0A0A]">
-              Download monthly report
-            </button>
-            <button className="border-white border-2 p-4 hover:border-[#CCFF00] rounded-lg w-64 bg-[#0A0A0A]">
-              Download yearly report
-            </button>
-          </div>
-          <div className="bg-[#307A4F] h-[493px] mt-5 rounded-lg flex flex-col justify-center items-center">
-            <Image
-              src={"/assets/svgs/Business-Report.svg"}
-              alt="Report icon"
-              width={300}
-              height={300}
-            />
-            <p className="font-bold mt-10">{randomTip}</p>
-          </div>
+        <div className="flex gap-5 mt-5">
+          <button
+            className="border-white border-2 p-4 hover:border-[#CCFF00] rounded-lg w-full bg-[#0A0A0A]"
+            aria-label="Download button"
+          >
+            Download monthly report
+          </button>
+          <button
+            className="border-white border-2 p-4 hover:border-[#CCFF00] rounded-lg w-full bg-[#0A0A0A]"
+            aria-label="Download button"
+          >
+            Download yearly report
+          </button>
         </div>
-      </section>
-    </div>
+        <div className="bg-[#307A4F] p-5 w-full mt-5 rounded-lg flex flex-col justify-center items-center">
+          <Image
+            src={"/assets/svgs/Business-Report.svg"}
+            alt="Report icon"
+            width={300}
+            height={300}
+          />
+          <p className="font-bold mt-2">{randomTip}</p>
+        </div>
+      </div>
+    </section>
   );
 }
