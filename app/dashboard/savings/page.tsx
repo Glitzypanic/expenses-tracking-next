9e7d70fc0becc75519d40229e648988fe8d9023e
glitzypanic <jose.fariasdv@gmail.com>
2024-08-19 22:27:57 -0400

    refactor: update savings page layout and styles

--- app/dashboard/savings/page.tsx
+++ app/dashboard/savings/page.tsx
@@ -8,36 +8,26 @@ import Balance from "@/app/components/ui/savings/Balance";
 
 export default function ExpensesPage() {
   return (
-    <div className=" col-span-2 h-full mx-5 mt-5">
-      <section className=" flex gap-5 rounded-lg w-fit">
-        <div className="bg-[#0A0A0A] rounded-lg p-5 w-[600px]">
-          <h2 className="text-3xl font-bold">Savings</h2>
+    <div>
+      <section className="flex md:flex-col lg:flex-row gap-5 rounded-lg">
+        <div className="bg-[#0A0A0A] rounded-lg p-5 w-full">
           <PieChart />
         </div>
-        <div className="flex flex-col justify-between">
+        <div className="flex flex-col gap-4 w-full">
           <AddFunds />
           <CurrGoal />
           <SavingCalc />
         </div>
-        <div className="">
+        <div className="w-full">
           <Tips />
         </div>
       </section>
-      <section className="flex mt-5">
-        <div className="mr-5 bg-[#0A0A0A] w-fit p-5 rounded-lg">
+      <section className="flex md:flex-col lg:flex-row my-5 gap-5 h-[280px]">
+        <div className="flex w-full items-center bg-[#0A0A0A] p-5 rounded-lg">
           <Balance />
         </div>
-        <div className="rounded-lg p-5 bg-[#0A0A0A] w-[900px]">
+        <div className="flex w-full rounded-lg p-5 bg-[#0A0A0A]">
           <Goals />
-          {/* <Calendar
-            aria-label="Date (Visible Month)"
-            visibleMonths={3}
-            classNames={{
-              gridBody: "bg-[#0A0A0A]",
-              base: "shadow-none",
-              gridWrapper: "bg-[#0A0A0A]",
-            }}
-          /> */}
         </div>
       </section>
     </div>
