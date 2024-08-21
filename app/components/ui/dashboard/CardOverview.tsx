d29296cdb0fc5663d0ee3eafff6d40316d22d09d
glitzypanic <jose.fariasdv@gmail.com>
2024-08-20 23:43:48 -0400

    refactor: Update CardOverview component layout and styles

--- app/components/ui/dashboard/CardOverview.tsx
+++ app/components/ui/dashboard/CardOverview.tsx
@@ -1,29 +1,35 @@
-export default function CardOverview({
-  title,
-  value,
-  percentage,
-  description,
-}: {
-  title: string;
-  value: string;
-  percentage: string;
-  description: string;
-}) {
+export default function CardOverview() {
   return (
-    <article>
-      <div className="bg-[#0A0A0A] rounded-lg p-5 flex flex-col">
-        <h3 className="text-2xl font-bold">{title}</h3>
+    <article className="flex p-5 rounded-lg bg-[#0A0A0A] gap-10">
+      <div className="flex flex-col">
+        <h3 className="text-2xl font-bold">Income</h3>
         <div className="flex items-center gap-10">
           <span className="text-5xl mt-5">
             <span className="text-[#CCFF00]">$</span>
-            {value}
+            7,400
           </span>
           <div className="text-[#9ca3af] text-xs font-semibold">
             <span className="">
-              <span className="">+</span>
-              {percentage}
+              <span className="">+ </span>
+              5,4
             </span>
-            <p>{description}</p>
+            <p>Last month</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col">
+        <h3 className="text-2xl font-bold">Expense</h3>
+        <div className="flex items-center gap-10">
+          <span className="text-5xl mt-5">
+            <span className="text-[#CCFF00]">$</span>
+            300
+          </span>
+          <div className="text-[#9ca3af] text-xs font-semibold">
+            <span className="">
+              <span className="">- </span>
+              5,4
+            </span>
+            <p>Last month</p>
           </div>
         </div>
       </div>
