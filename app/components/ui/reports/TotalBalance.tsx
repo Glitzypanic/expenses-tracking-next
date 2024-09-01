c78ec9232c0b2b6067a6f73c2a4ed7932e454551
glitzypanic <jose.fariasdv@gmail.com>
2024-08-31 23:39:28 -0400

    Refact: add amount prop

--- app/components/ui/reports/TotalBalance.tsx
+++ app/components/ui/reports/TotalBalance.tsx
@@ -1,11 +1,11 @@
-export default function TotalBalance() {
+export default function TotalBalance({ amount: amount }: any) {
   return (
     <section className="w-full bg-[#0A0A0A] p-5 rounded-lg">
       <div>
         <h2>Total Balance</h2>
       </div>
       <div className="flex flex-col">
-        <span className="text-4xl font-bold">$7400</span>
+        <span className="text-4xl font-bold">${amount}</span>
         <span className="text-sm text-green-500">+5.4% last month</span>
       </div>
     </section>
